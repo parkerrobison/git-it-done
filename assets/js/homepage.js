2250a8e7ea858d3cd8d5fa76c241c54bacd48b01
probison <parker.robison@hotmail.com>
2020-05-31 23:38:35 -0600

    added form and a submit listener to receive input for getUserRepos()

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,11 +4,30 @@ var getUserRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        // this formats the response to a readable array
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos("parkerrobison");
+getUserRepos();
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        // this sets the value as whatever the user types into the form. Which gets stored in var username.
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
