f00c9e9435d6c7091fc5e725ff6f2d845dd43503
probison <parker.robison@hotmail.com>
2020-06-01 21:21:33 -0600

    removed a function call that was producing an undefined div

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -25,8 +25,6 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos();
-
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
